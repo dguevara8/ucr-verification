cc9cebad0993d345cd0203ed14601f5cae5dde8e
Oscar Rojas Rojas <oscarrojasrojas6xc@gmail.com>
2026-05-06 19:22:51 -0600

    Volviendo al commit necesario para el proyecto
    Signed-off-by: Oscar Rojas Rojas <oscarrojasrojas6xc@gmail.com>

--- primeravance/Ambiente/testbench_eda.sv
+++
@@ -1,8 +0,0 @@
-`include "interface.sv"
-`include "stimulus.sv"
-`include "scoreboard.sv"
-`include "driver.sv"
-`include "monitor.sv"
-`include "env.sv"
-`include "test.sv"
-`include "top.sv"
--- primeravance/Ambiente/testcase.sv
+++
@@ -1,9 +0,0 @@
-program testcase (ifc_ram ifc_ram_obj);
-
-    env env_obj = new(ifc_ram_obj)
-
-    initial begin
-
-    end
-
-endprogram
